223e4bf95be7027f064f1d2ea3ee5ce90ff86e93
Ira <vishneviyshturman@gmail.com>
2023-11-08 20:46:25 +0300

    fix model

--- hw_ss/model/__init__.py
+++ hw_ss/model/__init__.py
@@ -1,5 +1,7 @@
 from hw_ss.model.baseline_model import BaselineModel
+from hw_ss.model.spex_model import SpexSimple
 
 __all__ = [
     "BaselineModel",
+    "SpexSimple"
 ]
--- hw_ss/model/spex_model.py
+++ hw_ss/model/spex_model.py
@@ -11,8 +11,8 @@ class SpeechEncoder(nn.Module):
         super().__init__()
         self.embed_dim = embed_dim
         self.short_encoder = nn.Conv1d(1, embed_dim, L, stride=L // 2)
-        self.middle_encoder = nn.Conv1d(1, embed_dim, L, stride=L // 2)
-        self.long_encoder = nn.Conv1d(1, embed_dim, L, stride=L // 2)
+        self.middle_encoder = nn.Conv1d(1, embed_dim, L2, stride=L // 2)
+        self.long_encoder = nn.Conv1d(1, embed_dim, L3, stride=L // 2)
         self.relu = nn.ReLU()
         self.L1 = L1
         self.L2 = L2
@@ -22,16 +22,18 @@ class SpeechEncoder(nn.Module):
         x1 = self.short_encoder(x)
         len_middle = (x1.shape[-1] - 1) * (self.L1 // 2) + self.L2
         len_long = (x1.shape[-1] - 1) * (self.L1 // 2) + self.L3
-        x2 = F.pad(x, (0, len_middle - x.shape[-1], "constant", 0))
-        x3 = F.pad(x, (0, len_long - x.shape[-1], "constant", 0))
+        x2 = F.pad(x, (0, len_middle - x.shape[-1]), "constant", 0)
+        x3 = F.pad(x, (0, len_long - x.shape[-1]), "constant", 0)
         x2 = self.middle_encoder(x2)
-        x3 = self.middle_encoder(x3)
+        x3 = self.long_encoder(x3)
         return self.relu(x1), self.relu(x2), self.relu(x3)
 
 
 class ResNetBlock(nn.Module):
     def __init__(self, in_channels, out_channels, max_pool=3):
         super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         self.first_layers = Sequential(
             nn.Conv1d(in_channels, out_channels, kernel_size=1, bias=False),
             nn.BatchNorm1d(out_channels),
@@ -39,14 +41,19 @@ class ResNetBlock(nn.Module):
             nn.Conv1d(out_channels, out_channels, kernel_size=1, bias=False),
             nn.BatchNorm1d(out_channels)
         )
+        if in_channels != out_channels:
+            self.last_conv = nn.Conv1d(
+                in_channels, out_channels, kernel_size=1, bias=False)
         self.last_layers = Sequential(
             nn.PReLU(),
             nn.MaxPool1d(max_pool)
         )
 
     def forward(self, x):
-        x_prime = x.copy()
+        x_prime = x
         x = self.first_layers(x)
+        if self.in_channels != self.out_channels:
+            x_prime = self.last_conv(x_prime)
         x = x + x_prime
         return self.last_layers(x)
 
@@ -56,18 +63,18 @@ class SpeakerEncoder(nn.Module):
         super().__init__()
         self.L1 = L1
         self.layers = Sequential(
-            self.layers.append(ChannelWiseLayerNorm(3*embed_dim)),
+            ChannelWiseLayerNorm(3*embed_dim),
             nn.Conv1d(3*embed_dim, embed_dim, 1),
             ResNetBlock(embed_dim, embed_dim),
             ResNetBlock(embed_dim, hidden),
             ResNetBlock(hidden, hidden),
             nn.Conv1d(hidden, embed_dim, 1),
-            nn.AvgPool1d((1, 3))
+            nn.AvgPool1d(3)
         )
 
     def forward(self, x1, x2, x3):
         x = torch.cat([x1, x2, x3], 1)
-        return self.layers(x)
+        return self.layers(x).sum(-1)
 
 
 class GlobalChannelLayerNorm(nn.Module):
@@ -90,11 +97,11 @@ class TCN(nn.Module):
     def __init__(self, in_channels, hidden, out_channels, kernel_size=3, dilation=1):
         super().__init__()
         self.layers = Sequential(
-            nn.Conv1D(in_channels, hidden, 1),
+            nn.Conv1d(in_channels, hidden, 1),
             nn.PReLU(),
             GlobalChannelLayerNorm(hidden),
             nn.Conv1d(hidden, hidden, kernel_size, groups=hidden,
-                      padding=dilation*(kernel_size-1), dilation=dilation),
+                      padding=dilation*(kernel_size-2), dilation=dilation),
             nn.PReLU(),
             GlobalChannelLayerNorm(hidden),
             nn.Conv1d(hidden, out_channels, 1)
@@ -109,7 +116,7 @@ class TCNResidual(TCN):
         super().__init__(in_channels, hidden, out_channels, kernel_size, dilation)
 
     def forward(self, x):
-        x_prime = x.copy()
+        x_prime = x
         x = self.layers(x)
         return x + x_prime
 
@@ -121,14 +128,14 @@ class TCNBlock(nn.Module):
         self.layers = []
         for i in range(B-1):
             self.layers.append(TCNResidual(in_channels, hidden, in_channels))
-        self.layers = Sequential(self.layers)
+        self.layers = Sequential(*self.layers)
 
     def forward(self, x, v):
         v = torch.unsqueeze(v, -1)
         v = v.repeat(1, 1, x.shape[-1])
-        x_prime = x.copy()
+        x_prime = x
         x = self.first(torch.cat([x, v], 1))
-        x + x_prime
+        x = x + x_prime
         return self.layers(x)
 
 
@@ -144,16 +151,16 @@ class ChannelWiseLayerNorm(nn.LayerNorm):
 
 
 class SpeakerExtractor(nn.Module):
-    def __init__(self, embed_dim, B=8, R=4):
+    def __init__(self, embed_dim, hidden, B=8, R=4):
         super().__init__()
         self.tail = Sequential(ChannelWiseLayerNorm(
-            3*embed_dim), nn.Conv1D(3*embed_dim, B, 1))
+            3*embed_dim), nn.Conv1d(3*embed_dim, hidden, 1))
         self.tcn = []
         for i in range(R):
-            self.tcn.append(TCNBlock(B))
-        self.head1 = Sequential(nn.Conv1D(B, embed_dim, 1), nn.ReLU())
-        self.head2 = Sequential(nn.Conv1D(B, embed_dim, 1), nn.ReLU())
-        self.head3 = Sequential(nn.Conv1D(B, embed_dim, 1), nn.ReLU())
+            self.tcn.append(TCNBlock(embed_dim, 2*embed_dim, embed_dim, B))
+        self.head1 = Sequential(nn.Conv1d(hidden, embed_dim, 1), nn.ReLU())
+        self.head2 = Sequential(nn.Conv1d(hidden, embed_dim, 1), nn.ReLU())
+        self.head3 = Sequential(nn.Conv1d(hidden, embed_dim, 1), nn.ReLU())
 
     def forward(self, y1, y2, y3, v):
         y = self.tail(torch.cat([y1, y2, y3], 1))
@@ -175,40 +182,39 @@ class ConvTrans1D(nn.ConvTranspose1d):
 
 
 class SpeechDecoder(nn.Module):
-    def __init__(self, embed_dim, L):
+    def __init__(self, embed_dim, L, L1, L2, L3):
         super().__init__()
         self.decoder1 = nn.ConvTranspose1d(
-            embed_dim, 1, kernel_size=L, stride=L//2, bias=True)
+            embed_dim, 1, kernel_size=L, stride=L//2)
         self.decoder2 = nn.ConvTranspose1d(
-            embed_dim, 1, kernel_size=L, stride=L//2, bias=True)
+            embed_dim, 1, kernel_size=L2, stride=L//2)
         self.decoder3 = nn.ConvTranspose1d(
-            embed_dim, 1, kernel_size=L, stride=L//2, bias=True)
+            embed_dim, 1, kernel_size=L3, stride=L//2)
 
     def forward(self, s1, s2, s3):
-        s1 = torch.unsqueeze(s1, 1)
-        s2 = torch.unsqueeze(s2, 1)
-        s3 = torch.unsqueeze(s3, 1)
         s1 = self.decoder1(s1)
-        s2 = self.decoder1(s2)
-        s3 = self.decoder1(s3)
-        return torch.squeeze(s1), torch.squeeze(s2), torch.squeeze(s3)
+        s2 = self.decoder2(s2)
+        s3 = self.decoder3(s3)
+        return torch.squeeze(s1), torch.squeeze(s2)[:, :s1.shape[-1]], torch.squeeze(s3)[:, :s1.shape[-1]]
 
 
 class SpexSimple(nn.Module):  # no classification head
-    def __init__(self, embed_dim=256, speaker_encoder_hidden=512, L=20, B=8, R=4, n_r=3, L1=20, L2=80, L3=160):
+    def __init__(self, embed_dim=256, hidden=256, speaker_encoder_hidden=512, L=20, B=8, R=4, n_r=3, L1=20, L2=80, L3=160):
         super().__init__()
         self.speech_encoder = SpeechEncoder(embed_dim, L, L1, L2, L3)
-        self.speaker_extractor = SpeakerEncoder(
+        self.speaker_encoder = SpeakerEncoder(
             embed_dim, speaker_encoder_hidden, L1)
-        self.speaker_encoder = SpeakerExtractor(embed_dim, B, R)
-        self.speech_decoder = SpeechDecoder(embed_dim, L)
+        self.speaker_extractor = SpeakerExtractor(embed_dim, hidden, B, R)
+        self.speech_decoder = SpeechDecoder(embed_dim, L, L1, L2, L3)
 
-    def forward(self, y, x):
-        X1, X2, X3 = self.speech_encoder(x)
-        Y1, Y2, Y3 = self.speech_encoder(y)
+    def forward(self, batch):
+        X1, X2, X3 = self.speech_encoder(batch["ref"])
+        Y1, Y2, Y3 = self.speech_encoder(batch["audio"])
+
         v = self.speaker_encoder(X1, X2, X3)
         M1, M2, M3 = self.speaker_extractor(Y1, Y2, Y3, v)
         S1 = Y1 * M1
         S2 = Y2 * M2
         S3 = Y3 * M3
-        return self.speech_decoder(S1, S2, S3)
+        predicted_audio = self.speech_decoder(S1, S2, S3)
+        return {"predicted_audio": predicted_audio}
