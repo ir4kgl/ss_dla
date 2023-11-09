412dbb08aa03f94ef7312044934c56d5c8f21aa9
Ira <vishneviyshturman@gmail.com>
2023-11-09 09:33:50 +0300

    add CE loss in init

--- hw_ss/loss/__init__.py
+++ hw_ss/loss/__init__.py
@@ -1,7 +1,9 @@
 from hw_ss.loss.SISDRLossWrapper import SISPDRLossWrapper
 from hw_ss.loss.MultiLoss import MultiLoss
+from hw_ss.loss.CELossWrapper import CELossWrapper
 
 __all__ = [
     "SISPDRLossWrapper",
-    "MultiLoss"
+    "MultiLoss",
+    "CELossWrapper"
 ]
