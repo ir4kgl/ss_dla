5d99486d99ad6aea3dc0cc788b471beced71cf40
Ira <vishneviyshturman@gmail.com>
2023-11-09 08:46:04 +0300

    turn on eval

--- hw_ss/trainer/trainer.py
+++ hw_ss/trainer/trainer.py
@@ -124,10 +124,10 @@ class Trainer(BaseTrainer):
                 break
         log = last_train_metrics
         # if epoch % 10 == 9:
-        #     for part, dataloader in self.evaluation_dataloaders.items():
-        #         val_log = self._evaluation_epoch(epoch, part, dataloader)
-        #         log.update(**{f"{part}_{name}": value for name,
-        #                       value in val_log.items()})
+        for part, dataloader in self.evaluation_dataloaders.items():
+            val_log = self._evaluation_epoch(epoch, part, dataloader)
+            log.update(**{f"{part}_{name}": value for name,
+                          value in val_log.items()})
 
         return log
 
