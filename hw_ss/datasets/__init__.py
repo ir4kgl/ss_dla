e86a789bd45b48b510d2382aa1fe0eed80e87ba7
Ira <vishneviyshturman@gmail.com>
2023-11-13 18:46:32 +0300

    add mixed general dataset

--- hw_ss/datasets/__init__.py
+++ hw_ss/datasets/__init__.py
@@ -1,4 +1,5 @@
 from hw_ss.datasets.librispeech_dataset import LibrispeechDataset
+from hw_ss.datasets.mixed_librispeech_dataset import MixedGeneralDataset
 from hw_ss.datasets.mixed_librispeech_dataset import MixedLibrispeechDataset
 
 __all__ = [
