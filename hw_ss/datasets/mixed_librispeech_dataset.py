c8d221aeddb4aa9c69d80fc9a41e3c7a29db0cb6
Ira <vishneviyshturman@gmail.com>
2023-11-13 18:30:29 +0300

    add general mixed dataset

--- hw_ss/datasets/mixed_librispeech_dataset.py
+++ hw_ss/datasets/mixed_librispeech_dataset.py
@@ -88,3 +88,86 @@ class MixedLibrispeechDataset(BaseDataset):
             "ref_len": ref_len,
             "target_id": target_id
         }
+
+
+class MixedGeneralDataset(BaseDataset):
+    def __init__(self,  data_dir, index_dir, *args, **kwargs):
+        self.indeces_map = {}
+        self.next_label = 0
+        self._data_dir = Path(data_dir)
+        self._index_dir = Path(index_dir)
+        index = self._get_or_load_index()
+        super().__init__(index, *args, **kwargs)
+
+    def _get_or_load_index(self):
+        index_path = self._index_dir / f"mixed_index.json"
+        if index_path.exists():
+            with index_path.open() as f:
+                index = json.load(f)
+        else:
+            index = self._create_index()
+            with index_path.open("w") as f:
+                json.dump(index, f, indent=2)
+        return index
+
+    def _create_index(self):
+        index = []
+
+        ref_files = sorted(
+            glob(os.path.join(self._data_dir / "refs", '*-ref.wav')))
+        mix_files = sorted(
+            glob(os.path.join(self._data_dir / "targets", '*-mixed.wav')))
+        target_files = sorted(
+            glob(os.path.join(self._data_dir / "mix", '*-target.wav')))
+
+        for i in range(len(ref_files)):
+            ref = ref_files[i]
+            mix = mix_files[i]
+            target = target_files[i]
+
+            target_id = int(target.split('/')[-1].split('_')[0])
+            noise_id = int(target.split('/')[-1].split('_')[1])
+
+            assert (target_id == int(ref.split('/')[-1].split('_')[0]))
+            assert (target_id == int(mix.split('/')[-1].split('_')[0]))
+            assert (noise_id == int(ref.split('/')[-1].split('_')[1]))
+            assert (noise_id == int(mix.split('/')[-1].split('_')[1]))
+
+            if not target_id in self.indeces_map.keys():
+                self.indeces_map[target_id] = self.next_label
+                self.next_label += 1
+            index.append(
+                {
+                    "path_target": target,
+                    "path_audio": mix,
+                    "path_ref": ref,
+                    "target_id": self.indeces_map[target_id]
+                }
+            )
+        return index
+
+    def __getitem__(self, ind):
+        data_dict = self._index[ind]
+        path_audio = data_dict["path_audio"]
+        path_ref = data_dict["path_ref"]
+        path_target = data_dict["path_target"]
+        target_id = data_dict["target_id"]
+        audio_wave = self.load_audio(path_audio)
+        ref_wave = self.load_audio(path_ref)
+        target_wave = self.load_audio(path_target)
+        audio_wave = self.process_wave(audio_wave)
+        ref_wave = self.process_wave(ref_wave)
+        target_wave = self.process_wave(target_wave)
+        ref_len = ref_wave.shape[-1]
+        audio_len = audio_wave.shape[-1]
+        target_len = target_wave.shape[-1]
+        return {
+            "audio": audio_wave,
+            "target": target_wave,
+            "ref": ref_wave,
+            "target_id": target_id,
+            "target_len": target_len,
+            "audio_len": audio_len,
+            "ref_len": ref_len,
+            "target_id": target_id
+        }
