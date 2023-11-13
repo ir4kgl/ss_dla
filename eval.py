3557732c5f3a995ba3093379509d8f0f237a0b62
Ira <vishneviyshturman@gmail.com>
2023-11-13 18:39:22 +0300

    add evaluation script

---
+++ eval.py
@@ -0,0 +1,139 @@
+import argparse
+import json
+import os
+from pathlib import Path
+
+import torch
+from tqdm import tqdm
+
+from hw_ss.utils.object_loading import get_dataloaders
+from hw_ss.utils.parse_config import ConfigParser
+
+import hw_ss.model as module_model
+
+from hw_ss.trainer import Trainer
+from hw_ss.metric import PESQMetric, SISPDRMetric
+
+DEFAULT_CHECKPOINT_PATH = Path(
+    "saved/models/default_config/1111_042016/checkpoint-epoch20.pth")
+
+
+def main(config, out_file):
+    logger = config.get_logger("test")
+
+    # define cpu or gpu if possible
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # setup data_loader instances
+    dataloaders = get_dataloaders(config)
+
+    # build model architecture
+    model = config.init_obj(config["arch"], module_model)
+    logger.info(model)
+
+    logger.info("Loading checkpoint: {} ...".format(config.resume))
+    checkpoint = torch.load(config.resume, map_location=device)
+    state_dict = checkpoint["state_dict"]
+    if config["n_gpu"] > 1:
+        model = torch.nn.DataParallel(model)
+    model.load_state_dict(state_dict)
+
+    # prepare model for testing
+    model = model.to(device)
+    model.eval()
+
+    sisdr = SISPDRMetric()
+    pesq = PESQMetric()
+
+    accum_sisdr = []
+    accum_pesq = []
+
+    with torch.no_grad():
+        for batch_num, batch in enumerate(tqdm(dataloaders["test"])):
+            batch = Trainer.move_batch_to_device(batch, device)
+            output = model(batch)
+            batch.update(output)
+            batch["sisdr"] = sisdr(batch)
+            batch["pesq"] = pesq(batch)
+            accum_sisdr.append(batch["sisdr"])
+            accum_pesq.append(batch["pesq"])
+            if len(accum_sisdr) > 100:
+                break
+    print("mean SI-SDR:\t", sum(accum_sisdr) / len(accum_sisdr))
+    print("mean PESQ:\t", sum(accum_pesq) / len(accum_pesq))
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(description="PyTorch Template")
+    args.add_argument(
+        "-c",
+        "--config",
+        default=None,
+        type=str,
+        help="config file path (default: None)",
+    )
+    args.add_argument(
+        "-r",
+        "--resume",
+        default=str(DEFAULT_CHECKPOINT_PATH.absolute().resolve()),
+        type=str,
+        help="path to latest checkpoint (default: None)",
+    )
+    args.add_argument(
+        "-d",
+        "--device",
+        default=None,
+        type=str,
+        help="indices of GPUs to enable (default: all)",
+    )
+    args.add_argument(
+        "-o",
+        "--output",
+        default="output.json",
+        type=str,
+        help="File to write results (.json)",
+    )
+    args.add_argument(
+        "-t",
+        "--test-data-folder",
+        default=None,
+        type=str,
+        help="Path to dataset",
+    )
+    args.add_argument(
+        "-b",
+        "--batch-size",
+        default=20,
+        type=int,
+        help="Test dataset batch size",
+    )
+    args.add_argument(
+        "-j",
+        "--jobs",
+        default=1,
+        type=int,
+        help="Number of workers for test dataloader",
+    )
+
+    args = args.parse_args()
+
+    # set GPUs
+    if args.device is not None:
+        os.environ["CUDA_VISIBLE_DEVICES"] = args.device
+
+    # first, we need to obtain config with model parameters
+    # we assume it is located with checkpoint in the same folder
+    model_config = Path(args.resume).parent / "config.json"
+    with model_config.open() as f:
+        config = ConfigParser(json.load(f), resume=args.resume)
+
+    # update with addition configs from `args.config` if provided
+    if args.config is not None:
+        with Path(args.config).open() as f:
+            config.config.update(json.load(f))
+
+    assert config.config.get("data", {}).get("test", None) is not None
+    config["data"]["test"]["batch_size"] = args.batch_size
+    config["data"]["test"]["n_jobs"] = args.jobs
+
+    main(config, args.output)
