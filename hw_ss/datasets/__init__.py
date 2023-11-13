a8dfbfe19e15009cc4239266a9200f4de2c67210
Ira <vishneviyshturman@gmail.com>
2023-11-13 20:35:33 +0300

    add configs

--- hw_ss/datasets/__init__.py
+++ hw_ss/datasets/__init__.py
@@ -1,9 +1,7 @@
-from hw_ss.datasets.librispeech_dataset import LibrispeechDataset
 from hw_ss.datasets.mixed_librispeech_dataset import MixedGeneralDataset
 from hw_ss.datasets.mixed_librispeech_dataset import MixedLibrispeechDataset
 
 __all__ = [
-    "LibrispeechDataset",
     "MixedLibrispeechDataset",
     "MixedGeneralDataset"
 ]
